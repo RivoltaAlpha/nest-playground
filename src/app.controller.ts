95c20718d54184de3f546b70fb6d9885342457d4
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-26 23:21:51 +0300

    Root files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // This is the root endpoint or https://localhost:3000/ method
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, OrdersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
