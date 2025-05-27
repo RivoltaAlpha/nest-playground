399b5d14afc5b9e3427e7864e9a57e2c1d934458
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-27 13:05:58 +0300

    DTO's

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,20 @@
-export class CreateOrderDto {}
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+interface OrderItem {
+        productId: string;
+        quantity: number;
+        price: number;
+    }
+export class CreateOrderDto {
+    @IsNotEmpty()
+    @IsString()
+    customerName: string;
+
+    @IsOptional()
+    @IsArray()
+    items: OrderItem[];
+
+    @IsNotEmpty()
+    @IsNumber()
+    totalAmount: number;
+}
