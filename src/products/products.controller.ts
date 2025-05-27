b43129ac0410da7ee81ba975b19ab1fb54cd6876
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-05-27 10:13:03 +0300

    CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,34 +8,35 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { PRODUCTS } from 'src/mocks/products.mock';
+import { IProduct } from './entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
-  }
-
-  @Get()
+  @Get('all')
   findAll() {
     return this.productsService.findAll();
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  
+  @Get('product/:id')
+  async findOne(@Param('id') id: string) {
+    const book = await this.productsService.findOne(+id);
+    return book
+  }
+  
+  @Post('create')
+  create(@Body() createProductDto: IProduct) {
+    return this.productsService.create(createProductDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  @Patch('update-product/:id')
+  update(@Param('id') id: string, @Body() updateProductDto: IProduct) {
     return this.productsService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
+  @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
